85e19a82f16c2224a746a95e3b9742ee964a6c96
RaamyAfifi <56026512+RaamyAfifi@users.noreply.github.com>
2019-12-09 11:31:58 +0100

    login.php/maincss/login.css
    testen of background-repeat: none; werkt

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="stylesheet/main.css" type="text/css"  rel="stylesheet">
+	<link href="stylesheet/login.css" type="text/css"  rel="stylesheet">
+	<title>
+		Login
+	</title>
+</head>
+	<body>
+		<div class="inlogBox">
+			<form action="login.php">
+				<input class="inlogForm" placeholder="Email...." type="text" name="">
+				<input class="inlogForm" placeholder="Wachtwoord...." type="password" name="password">
+				<input class="loginButton" type="submit" name="submit" value="login">
+			</form>
+		</div>
+
+	</body>
+</html>

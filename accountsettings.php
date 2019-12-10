2d6adcd933a9417cf257863aaaecd0fd1d7641e3
KoenSomsen <koen.somsen@student.nhlstenden.com>
2019-12-10 11:19:02 +0100

    accountsettings.php added

---
+++ accountsettings.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="stylesheet/main.css" type="text/css"  rel="stylesheet">
+	<link href="stylesheet/accountsettings.css" type="text/css"  rel="stylesheet">
+	<title>
+		Account settings
+	</title>
+</head>
+	<body>
+		<div class="flex-container">
+			<form action="accountsettings.php">
+				<div class="flex-item"><input class="changeitems" placeholder="Username" type="text" name=""></div>
+				<div class="flex-item"><input class="changeitems" placeholder="Email" type="text" name=""></div>
+				<div class="flex-item"><input class="changebutton" type="submit" name="submit" value="change"></div>
+			</form>
+		</div>
+	</body>
+</html>

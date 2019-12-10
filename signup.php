cc2c79caf72b6897e25916767787a206878f66fe
KoenSomsen <koen.somsen@student.nhlstenden.com>
2019-12-10 12:57:08 +0100

    added signup.php

--- signup.php
+++ signup.php
@@ -3,9 +3,9 @@
 	<head>
 		<meta charset="utf-8">
 		<link href="stylesheet/main.css" type="text/css"  rel="stylesheet">
-		<link href="stylesheet/accountsettings.css" type="text/css"  rel="stylesheet">
+		<link href="stylesheet/signup.css" type="text/css"  rel="stylesheet">
 		<title>
-			Account settings
+			Sign Up
 		</title>
 	</head>
 	<body>
@@ -13,7 +13,9 @@
 			<form action="accountsettings.php">
 				<div class="flex-item"><input class="changeitems" placeholder="Username" type="text" name=""></div>
 				<div class="flex-item"><input class="changeitems" placeholder="Email" type="text" name=""></div>
-				<div class="flex-item"><input class="changebutton" type="submit" name="submit" value="Change"></div>
+				<div class="flex-item"><input class="changeitems" placeholder="Password" type="password" name=""></div>
+				<div class="flex-item"><input class="changeitems" placeholder="Confirm password" type="password" name=""></div>
+				<div class="flex-item"><input class="changebutton" type="submit" name="submit" value="Submit"></div>
 			</form>
 		</div>
 	</body>

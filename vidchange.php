a36b2880c15739f59542bcce3121da2d21b2145f
qwertyoriuop <41389168+qwertyoriuop@users.noreply.github.com>
2019-12-13 09:47:31 +0100

    Update vidchange.php

--- vidchange.php
+++ vidchange.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="stylesheet/main.css" type="text/css"  rel="stylesheet">
+	<link href="stylesheet/vidchange.css" type="text/css"  rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
@@ -12,7 +14,7 @@
     </div>
     <div class="content">
         <div class="slides">
-        <a href=/vid>
+        <a >
         </div>
         <div class="sidebar">
         </div>

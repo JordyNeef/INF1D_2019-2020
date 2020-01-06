1c292869afbb37bb7b5d8fbed7a8373b8ca8179a
Jordy Neef <43172462+JordyNeef@users.noreply.github.com>
2020-01-06 12:30:39 +0100

    video wijzigen/ verwijderen af

--- videoaanpassen.php
+++ videoaanpassen.php
@@ -1,31 +1,96 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <?php require 'config/functions.php';?>
+        <?php require 'config/conn.php';?>
+        <meta charset="UTF-8">
+        <link href='https://fonts.googleapis.com/css?family=Alata' rel='stylesheet'/>
         <title>Beheer</title>
+        <link type="text/css" rel="stylesheet" href="stylesheet/main.css"/>
         <link href="stylesheet/admin.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div id="containersubmit">
             <div id="containersubmitlogo">
-                <img src="img/logo.png" alt="logo">
+                <?php
+                    headerBar();
+                    navBar();
+                ?>
             </div>
             <div id="form">
-                <form action="" method="post">
-                    <h1>Video Aanpassen</h1>
-                    <div id="formsubmit">
-                        <div id="submitrowleft">
-                            <input type="text" name="videourl" placeholder="videourl..." class="textfield">
-                            <input type="text" name="titel" placeholder="titel van de video..." class="textfield">
-                            <input type="text" name="maker" placeholder="Naam van de maker..." class="textfield">
-                        </div>
-                        <div id="submitrowright">
-                            <input type="text" name="catagorie" placeholder="catagorie..." class="textfield">
-                            <textarea name="beschrijving" placeholder="beschrijving..."></textarea>
-                            <input type="submit" name="submit" value="Voeg video toe..." id="submitbutton">
-                        </div>
+                
+                    <?php
+                    if($conn === FALSE){
+                        echo "Er kon niet worden verbonden met de database.";
+                    } else{
+                        $videoId = filter_input(INPUT_GET, 'videoid', FILTER_SANITIZE_SPECIAL_CHARS);
+                        $table = "video";
+                        $selectQeury = "SELECT playbackid, titel, beschrijving, UploadedBy, videocatid, leeftijd FROM $table WHERE videoid = $videoId";
+                        if($stmt = mysqli_prepare($conn, $selectQeury)){
+                            mysqli_execute($stmt);
+                            mysqli_stmt_bind_result($stmt, $pId, $titel, $beschrijving, $uploadBy, $vcId, $leeftijdR);
+                            mysqli_stmt_store_result($stmt);
+                            if(mysqli_stmt_num_rows($stmt) == 0){
+                                echo "deze videoid is niet bekend, kies een andere.";
+                            } else {
+                                while(mysqli_stmt_fetch($stmt)){
+                                   echo '<form action="" method="post">
+                                   <h1>Video Aanpassen</h1>
+                                   <div id="formsubmit">
+                                       <div id="submitrowleft">
+                                           <input type="text" name="videourl" value="'.$pId.'" class="textfield">
+                                           <input type="text" name="titel" value="'.$titel.'" class="textfield">
+                                           <input type="text" name="maker" value="'.$uploadBy.'" class="textfield">
+                                       </div>
+                                       <div id="submitrowright">
+                                           <input type="text" name="categorie" value="'.$vcId.'" class="textfield">
+                                           <textarea name="beschrijving">'.$beschrijving.'</textarea>
+                                           <input type="submit" name="wijzig" value="Voeg video toe..." id="submitbutton">
+                                       </div>';
+                                }
+                                echo "</table>";
+                                if(isset($_POST['wijzig'])){
+                                    if(empty($_POST["videourl"])){
+                                        echo "Voer een videourl in.";
+                                    }
+                                    if(empty($_POST["titel"])){
+                                        echo "Voer een titel in.";
+                                    }
+                                    if(empty($_POST["maker"])){
+                                        echo "Voer een maker in.";
+                                    }
+                                    if(empty($_POST["categorie"])){
+                                        echo "Voer een categorie in.";
+                                    }
+                                    if(empty($_POST["beschrijving"])){
+                                        echo "Voer een beschrijving in.";
+                                    } else{
+                                        $videoUrl = filter_input(INPUT_POST, 'videourl', FILTER_SANITIZE_SPECIAL_CHARS);
+                                        $titel = filter_input(INPUT_POST, 'titel', FILTER_SANITIZE_SPECIAL_CHARS);
+                                        $maker = filter_input(INPUT_POST, 'maker', FILTER_SANITIZE_SPECIAL_CHARS);
+                                        $categorie = filter_input(INPUT_POST, 'categorie', FILTER_SANITIZE_SPECIAL_CHARS);
+                                        $beschrijving = filter_input(INPUT_POST, 'beschrijving', FILTER_SANITIZE_SPECIAL_CHARS);
+                                        $updateQeury = "UPDATE video SET playbackid= '$videoId', titel= '$titel', beschrijving='$beschrijving', uploadedby= '$maker' WHERE videoid=$videoId";
+                                        if(mysqli_query($conn, $updateQeury)){
+                                            echo "<p>video is met succes ge-update.</p><p><a href='videowijzigen.php'>Video wijzigen</a></p>";
+                                        } else{
+                                            echo "er ging iets fout tijdens het updaten" . mysqli_error($conn);
+                                        }
+                                        
+                                    }
+                                }
+                            }
+                            mysqli_stmt_close($stmt);
+                        } else {
+                            echo "Something went wrong with the qeury.";
+                        }
+                        mysqli_close($conn);  
+                    }
+                    ?>
                     </div>
                 </form>
             </div>
         </div>
     </body>
+    <?php navScript();?>
 </html>
--- videowijzigen.php
+++ videowijzigen.php
@@ -1,27 +1,71 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <?php require 'config/functions.php'; ?>
+        <?php require 'config/conn.php'; ?>
+        <meta charset="UTF-8">
+        <link href='https://fonts.googleapis.com/css?family=Alata' rel='stylesheet'/>
         <title>Beheer</title>
+        <link type="text/css" rel="stylesheet" href="stylesheet/main.css"/>
         <link href="stylesheet/admin.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div id="containersubmit">
             <div id="containersubmitlogo">
-                <img src="img/logo.png" alt="logo">
+                <?php
+                    headerBar();
+                    navBar();
+                ?>
             </div>
             <div id="form">
-                <form action="mailto:email@email.com" method="post">
+                <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
                     <h1>Wijzig een video</h1>
                     <div id="wijzigvideo">
-                            <input type="text" name="videourl" placeholder="videourl of titel van een video..." class="textfield">
+                            <input type="number" name="videoid" placeholder="videoid van een video..." class="textfield">
                             <div id="videowijzigenbuttons">
                                 <input type="submit" name="verwijder" value="Video verwijderen" id="verwijderbutton">
                                 <input type="submit" name="wijzig" value="wijzig video" id="wijzigbutton">
                             </div>
+                    </div>
+                    <div id="formresult">
+                        <?php
+                            if(isset($_POST['verwijder'])){
+                                if(empty($_POST['videoid'])){
+                                    echo "kies welke video moet worden verwijderd.";
+                                } else {
+                                    if($conn === FALSE){
+                                        echo "Er kon niet worden verbonden met de database.";
+                                    } else{
+                                        $videoId = filter_input(INPUT_POST, 'videoid', FILTER_SANITIZE_SPECIAL_CHARS);
+                                        $table = "video";
+                                        $verwijderQeury = "DELETE FROM video WHERE videoid=$videoId";
+                                        if(mysqli_query($conn, $verwijderQeury)){
+                                            echo "De video is met succes verwijderd.";
+                                        } else{
+                                            echo "Er ging iets fout met het verwijderen van de video" . mysqli_error($conn);
+                                        }
+                                        mysqli_close($conn);
+                                    }  
+                                }
+                            }
+                            if(isset($_POST['wijzig'])){
+                                if(empty($_POST['videoid'])){
+                                    echo "kies welke video moet worden verwijderd.";
+                                } else {
+                                    if($conn === FALSE){
+                                        echo "Er kon niet worden verbonden met de database.";
+                                    } else{
+                                        $videoId = filter_input(INPUT_POST, 'videoid', FILTER_SANITIZE_SPECIAL_CHARS);
+                                        header('Location: videoaanpassen.php?videoid= '.$videoId.'');
+                                    }
+                                    mysqli_close($conn);  
+                                } 
+                            }
+                        ?>
                     </div>  
                 </form>
             </div>
         </div>
-
     </body>
+    <?php navScript();?>
 </html>

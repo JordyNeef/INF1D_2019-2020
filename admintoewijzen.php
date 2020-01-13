f3555835c9328fffcf4fff7124cabcf89fc1915e
Jordy Neef <43172462+JordyNeef@users.noreply.github.com>
2020-01-13 10:32:16 +0100

    admin toewijzen

---
+++ admintoewijzen.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<?php require 'config/functions.php'; ?>
+<?php require 'config/conn.php'; ?>
+<html>
+    <head>
+        <title>Admin Portal</title>
+        <link href='https://fonts.googleapis.com/css?family=Alata' rel='stylesheet'/>
+        <link type="text/css" rel="stylesheet" href="stylesheet/admin.css" />
+        <link type="text/css" rel="stylesheet" href="stylesheet/main.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div id="containersubmit">
+        <div id="containersubmitlogo">
+                <?php
+                    headerBar();
+                    navBar();
+                ?>
+            </div>
+            <div id="form">
+                <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
+                    <h1>Admin toewijzen</h1>
+                    <div id="wijzigvideo">
+                        <input type="number" name="gebruikerid" class="textfield">
+                        <div id="videowijzigenbuttons">
+                            <input type="submit" name="toewijzen" value="Admin toevoegen!" id="toewijzbutton">
+                        </div>
+                        <div id="toewijzresult">
+                    <?php
+                        if(isset($_POST['toewijzen'])){
+                            if(empty($_POST['gebruikerid'])){
+                                echo "Kiez welke gebruiker u admin wilt maken.";
+                            } else{
+                                $gebruikerTable = "gebruiker";
+                                $gebruikerId = filter_input(INPUT_POST, 'gebruikerid', FILTER_SANITIZE_SPECIAL_CHARS);;
+                                $adminQuery = "UPDATE $gebruikerTable SET admin=1 WHERE gebruikerid=$gebruikerId";
+                                if(mysqli_query($conn, $adminQuery)){
+                                    echo "De gebruiker is nu een admin.";
+                                } else {
+                                    echo "er ging iets fout: De gebruiker kon geen admin worden gemaakt.";
+                                }
+                            }
+                        }
+                    ?>
+                </div>
+                    </div>
+                </form>
+                
+            </div>
+        </div>
+    </body>
+    <?php navScript();?>
+</html>

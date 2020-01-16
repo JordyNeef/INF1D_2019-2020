b96336969367062666e844c8cac4316b8cc51cc3
Maartje Arnoldus <maartje.arnoldus@hotmail.com>
2020-01-16 15:24:46 +0100

    uitleg bij code en werkt met youtube url

--- config/video_thumbnail.php
+++ config/video_thumbnail.php
@@ -12,36 +12,45 @@ function frontvidthumb() {
         if (mysqli_stmt_execute($stmt)) {
             mysqli_stmt_bind_result($stmt, $playbackid, $titel, $beschrijving, $uploadedby, $leeftijd, $categorieid, $videoid);
             while (mysqli_stmt_fetch($stmt)) {
-                $url = "https://img.youtube.com/vi/" . $playbackid . "/0.jpg";
-                $informatieArray = array($url, $titel, $beschrijving, $uploadedby, $leeftijd, $categorieid, $videoid, $playbackid);
+                explode( '?=', $playbackid);
+                $playbackid1 = str_replace("https://www.youtube.com/watch?v=","",$playbackid);
+                $url = "https://img.youtube.com/vi/" . $playbackid1 . "/0.jpg";
+                $informatieArray = array($url, $titel, $beschrijving, $uploadedby, $leeftijd, $categorieid, $videoid, $playbackid1);
                 array_push($videoArray, $informatieArray);
                 $i++;
             }
         }
         mysqli_stmt_close($stmt);
         while ($a < $i) {
+//            <a> tag die als achtergrond plaatje de thumbnail heeft
+//            en een onklik event die de functie popup uitvoert (openen van videospeler)
+//            functie ziet er uit als: popup(playbackid, titel, beschrijving); (geef je mee voor het displayen in de popup in het text vak)
             echo"<a class='video' onclick='popup(\"".$videoArray[$a][7]."\",\"".$videoArray[$a][1]."\",\"".$videoArray[$a][2]."\")'";
-            echo $videoArray[$a][6];
-            echo" style='background-image: url(\"" . $videoArray[$a][0] . "\")'><div class='videoInfo'>"
+            echo" style='background-image: url(\"" . $videoArray[$a][0] . "\")'>";
+            //div in de a voor het displayen van info als je er over hovered
+            echo"<div class='videoInfo'>"
             . "<h3 class='titel'>" . $videoArray[$a][1] . "</h3>"
             . "<h3 class='categorie'>Categorie(en):<br>";
+//            scheid de categorieids van elkaar
             $categorieArray = explode(',', $videoArray[$a][5]);
+//            echo alle categorieen waar een filmpje onder valt
             foreach ($categorieArray as $categorie) {
+//                maak van de string een int (want hij zag het als string)
                 $cat = intval($categorie);
                 $categorieStatement = "SELECT naam FROM `categorie` WHERE categorieid = " . $cat;
-                if ($stmt1 = mysqli_prepare($conn, $categorieStatement)) {
-                    mysqli_stmt_execute($stmt1);
-                    mysqli_stmt_bind_result($stmt1, $categorieNaam);
-                    mysqli_stmt_fetch($stmt1);
+                if ($stmt = mysqli_prepare($conn, $categorieStatement)) {
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_bind_result($stmt, $categorieNaam);
+                    mysqli_stmt_fetch($stmt);
                     echo $categorieNaam . " ";
-                    mysqli_stmt_close($stmt1);
+                    mysqli_stmt_close($stmt);
                 } else {
                     echo"prepare failed";
                 }
             }
             echo "</h3><h3 class='beschrijving'>Beschrijving:<br>" . $videoArray[$a][2] . "</h3>"
             . "<h3 class='leeftijd'>Leeftijd:<br>" . $videoArray[$a][4] . "</h3>"
-            . "<h3 class='likes'>Beoordeling:<br>sdfghj</h3></div></a>";
+            . "<h3 class='likes'>Beoordeling:<br>moet nog toegevoegd worden</h3></div></a>";
             $a++;
         }
     } else {
